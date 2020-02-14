928056a0575ae437a8ff03f0290d854fff59cd7e
anki-code <anki-code>
2020-02-14 17:54:58 +0300

    #8 Testing environment

--- xonssh_xxh/settings.py
+++ xonssh_xxh/settings.py
@@ -6,10 +6,6 @@ global_settings = {
 
 if __name__ == "__main__":
 
-    for e in ['XXH_HOME', 'PIP_TARGET', 'PYTHONPATH']:
-        if e in os.environ:
-            global_settings[e] = os.environ[e]
-
     if len(sys.argv) > 1:
         setting_name = sys.argv[1]
         if setting_name in global_settings:
