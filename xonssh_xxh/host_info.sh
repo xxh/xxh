e5bb03df0963d05352c349f0c65419b4593cd210
anki-code <anki-code>
2020-02-15 00:07:21 +0300

    #2 Review and reduce count of information connections before final connect

--- xonssh_xxh/host_info.sh
+++ xonssh_xxh/host_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 xxh_home_realpath=`realpath _xxh_home_`
-mkdir -p $xxh_home_realpath
+mkdir -p $xxh_home_realpath $xxh_home_realpath/plugins
 
 settings_path=$xxh_home_realpath/settings.py
 xxh_version=`[ "$(ls -A $xxh_home_realpath)" ] && echo "0" || echo "-1"`
@@ -11,7 +11,9 @@ fi
 
 echo xxh_home_realpath=$xxh_home_realpath
 echo xxh_version=$xxh_version
+echo xxh_home_freespace=`df -k --output=avail $xxh_home_realpath | tail -n1`
+echo xxh_plugins_rc=`find $xxh_home_realpath/plugins | grep xonshrc.xsh`
+
 echo bash=`command -v bash`
 echo rsync=`command -v rsync`
 echo scp=`command -v scp`
-echo xxh_home_freespace=`df -k --output=avail $xxh_home_realpath | tail -n1`
