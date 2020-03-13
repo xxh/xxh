cdb3860cc5c699c332960f8c6e9f43e6689729b1
anki-code <anki-code>
2020-03-13 17:44:01 +0300

    0.5.4

--- xxh.zsh
+++ xxh.zsh
@@ -1,8 +1,14 @@
+#
+# This entrypoint is to allow xxh getting current environment variables
+# and pass some of them to xxh session to seamless transition to host.
+#
+# Usage in zsh: source xxh.zsh [ordinary xxh arguments]
+#
 d=`declare -p 2>/dev/null`
 if [ ! $d ]; then
   echo  "\nThis entrypoint is to allow xxh getting current environment variables"
-  echo  "and pass some of them to xxh session.\n"
-  echo  "Usage in zsh: source xxh.sh [ordinary xxh arguments]\n"
+  echo  "and pass some of them to xxh session to seamless transition to host.\n"
+  echo  "Usage in zsh: source xxh.zsh [ordinary xxh arguments]\n"
 else
   XXH_SH_ENV=$d xxh +E +s xxh-shell-zsh "$@"
 fi
