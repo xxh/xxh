a286b2c5e1136c08500060a3a41e068452491e03
Andy Kipp <anki-code@users.noreply.github.com>
2021-03-14 18:08:08 +0300

    Fix pexpect

--- xxh_xxh/xxh.py
+++ xxh_xxh/xxh.py
@@ -160,7 +160,7 @@ class xxh:
                 else:
                     sess.sendline(key_password)
 
-            if pattern == [3,4]:
+            if pattern in [3,4]:
                 # Expected:
                 #   <host>`s password:
                 if host_password is None:
